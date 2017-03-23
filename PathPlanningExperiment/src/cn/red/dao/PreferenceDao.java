16a9dc6a52d437ca456c9eb181e8bf084f248454
windofred <1342670786@qq.com>
2017-03-23 17:01:16 +0800

    3月23日第二次提交 用户的评分和停留时间降序排序

---
+++ PathPlanningExperiment/src/cn/red/dao/PreferenceDao.java
@@ -0,0 +1,12 @@
+package cn.red.dao;
+
+public class PreferenceDao {
+	
+	
+	
+	//从数据库表中读取记录
+	public static void readPreference() {
+		
+	}
+	
+}
--- PathPlanningExperiment/src/cn/red/domain/UserTest.java
+++ PathPlanningExperiment/src/cn/red/domain/UserTest.java
@@ -1,44 +1,81 @@
 package cn.red.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
-import cn.red.undirectional.Vertex;
+import cn.red.utils.JDBCTools;
+
 
 public class UserTest {
 	
+	private Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	List<UserPreference> userPreferencesList = null;
+	
 	@Test
 	public void test1() {
-		
-		//Map<User,Vertex> map = new HashMap<User,Vertex>();
-		
-		Map<User, Map<Vertex,Integer>> map = new HashMap<User, Map<Vertex,Integer>>();
-
-		//用户1
-		User user1 = new User();
-		user1.setId("1");
-		user1.setName("Jack");
-	
-		//用户1分别对特征点1,2,3的评分
-		float[] user1_score = new float[]{0.1f, 0.2f, 0.3f};
-		
-		//特征点1
-		Vertex vertex1 = new Vertex();
-		vertex1.setId("1001");
-		vertex1.setName("room1001");
-		
-		//特征点2
-		Vertex vertex2 = new Vertex();
-		vertex2.setId("1002");
-		vertex2.setName("room1002");
-		
-		//特征点3
-		Vertex vertex3 = new Vertex();
-		vertex3.setId("1003");
-		vertex3.setName("room1003");
-		
+		try {
+			//获取数据库的连接
+			connection = JDBCTools.getConnection();
+			String sql = "select * from preference order by grade+retention desc";
+			preparedStatement = connection.prepareStatement(sql);
+			//preparedStatement.setInt(1, 3);
+			//获取结果集
+			resultSet = preparedStatement.executeQuery();
+			
+			UserPreference userPreference = null;
+			userPreferencesList = new ArrayList<UserPreference>();//初始化
+			
+			while(resultSet.next()) {
+				
+				userPreference = new UserPreference();
+				
+				int id = resultSet.getInt("id");
+				String userId = resultSet.getString("user_id");
+				String vertexId = resultSet.getString("vertex_id");
+				float grade = resultSet.getFloat("grade");
+				int retention = resultSet.getInt("retention");
+				
+				userPreference.setId(id);
+				userPreference.setUserId(userId);
+				userPreference.setVertexId(vertexId);
+				userPreference.setGrade(grade);
+				userPreference.setRetention(retention);
+				
+				userPreferencesList.add(userPreference);
+				
+				/*System.out.println("用户id: " + userId + " 节点id: " + vertexId + " 评分：" 
+									+ grade + " 停留时间：" + retention);*/
+			}
+			
+			//遍历userPreferencesList
+			for(UserPreference u : userPreferencesList) {
+				System.out.println(u);
+			}
+			
+			//获得用户的评分和用户的停留时间
+			int grade_length = userPreferencesList.size();
+			float[] grades = new float[grade_length];
+			int[] retentions = new int[grade_length];
+			
+			for(int i=0; i<grade_length; i++) {
+				grades[i] = userPreferencesList.get(i).getGrade();
+				retentions[i] = userPreferencesList.get(i).getRetention();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTools.releaseDB(resultSet, preparedStatement, connection);
+		}
 		
 	}
 	
